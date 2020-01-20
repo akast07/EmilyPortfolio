7e8e70a8159e092d003bf1868b96f93db3994173
akast <dev.alejandrocastillo@gmail.com>
2020-01-20 16:55:01 -0600

    idk what broke

--- js/EmJava.js
+++ js/EmJava.js
@@ -14,6 +14,7 @@ function everyImageContainer(){
 
 function autoHeightautoWidthImg(){
   let AllImageLi = document.getElementsByClassName('image');
+  console.log(AllImageLi);
   let uniqueImage;
   let elementAutoHeight;
 
